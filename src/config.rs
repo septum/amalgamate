8582eeef8edd465a93048b1fc4824161244fa61a
Orlando Valverde <orlando.jesig.va@gmail.com>
2022-06-20 17:30:44 -0500

    Modify the window mode in the configuration

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,7 @@
-use bevy::prelude::{Plugin as BevyPlugin, *};
+use bevy::{
+    prelude::{Plugin as BevyPlugin, *},
+    window::WindowMode,
+};
 
 pub struct Plugin;
 
@@ -6,6 +9,7 @@ impl BevyPlugin for Plugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(WindowDescriptor {
             title: "amalgamate".to_string(),
+            mode: WindowMode::BorderlessFullscreen,
             ..Default::default()
         });
     }
